57b79e79fb1f92f64e81b41c414cfe6dc3a9e7e2
Rowin Schmidt <rowinschmidt@gmail.com>
2022-04-24 01:32:05 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-// JavaScript Document
-console.log("Howdy!");
+// GAMEBOY --------------------------------
+var gameBoy = document.querySelector('main > article:nth-of-type(1)');
+var gameBoyStartButton = document.querySelector('main > article:nth-of-type(1) > section > section:nth-of-type(3) ul li:nth-of-type(3) button:last-of-type');
+gameBoyStartButton.addEventListener('mouseup', gameBoyMenuOpen);
+
+gameBoyStartButton.onmousedown = function() {
+    this.classList.add('pressed');
+}
+
+function gameBoyMenuOpen() {
+    this.classList.remove('pressed');
+    gameBoy.classList.add('menu-active');
+}
