ceecab6b1aa40d196b4cff3251b8cae32486f0b9
Rowin Schmidt <rowinschmidt@gmail.com>
2022-04-28 13:10:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ var gameBoy = document.querySelector('main > article:nth-of-type(1)');
 var gameBoyStartButton = document.querySelector('main > article:nth-of-type(1) > section:nth-of-type(1) > section:nth-of-type(3) > ul > li:nth-of-type(3) button:last-of-type');
 var gameBoyMenuButton1 = document.querySelector('main > article:nth-of-type(1) > section:nth-of-type(2) ol li:nth-of-type(1) button');
 var gameBoyMenuButton2 = document.querySelector('main > article:nth-of-type(1) > section:nth-of-type(2) ol li:nth-of-type(2) button');
-// var gameBoyMenuButton3 = document.querySelector('main > article:nth-of-type(1) > section:nth-of-type(2) ol li:nth-of-type(3) button');
+var gameBoyMenuButton3 = document.querySelector('main > article:nth-of-type(1) > section:nth-of-type(2) ol li:nth-of-type(3) button');
 gameBoyStartButton.addEventListener('mouseup', gameBoyMenuOpen);
 gameBoyMenuButton1.addEventListener('click', gameBoyMenuClose);
 
@@ -38,7 +38,7 @@ var DSLite = document.querySelector('main > article:nth-of-type(2)');
 var DSLiteStartButton = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(3) > section:last-of-type ul li:first-of-type button');
 var DSLiteMenuButton1 = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(2) ol li:nth-of-type(1) button');
 var DSLiteMenuButton2 = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(2) ol li:nth-of-type(2) button');
-// var DSLiteMenuButton3 = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(2) ol li:nth-of-type(3) button');
+var DSLiteMenuButton3 = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(2) ol li:nth-of-type(3) button');
 DSLiteStartButton.addEventListener('mouseup', DSLiteMenuOpen);
 DSLiteMenuButton2.addEventListener('click', DSLiteMenuClose);
 
@@ -94,38 +94,127 @@ function bootDSLiteAudio() {
     }
 }
 
+// D-PAD INTERACTION
+var DSLiteDPad = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(1) > ul');
+var DSLiteDPadLeft = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(1) > ul li:nth-of-type(1)');
+var DSLiteDPadUp = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(1) > ul li:nth-of-type(2)');
+var DSLiteDPadRight = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(1) > ul li:nth-of-type(3)');
+var DSLiteDPadDown = document.querySelector('main > article:nth-of-type(2) > section:nth-of-type(1) > section:nth-of-type(3) > section:nth-of-type(1) > ul li:nth-of-type(4)');
+DSLiteDPadLeft.onmousedown = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatey(-5deg) scale(.8)';
+}
+DSLiteDPadLeft.onmouseup = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatey(0deg) scale(.8);';
+}
+DSLiteDPadUp.onmousedown = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatex(5deg) scale(.8)';
+}
+DSLiteDPadUp.onmouseup = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatex(0deg) scale(.8);';
+}
+DSLiteDPadRight.onmousedown = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatey(5deg) scale(.8)';
+}
+DSLiteDPadRight.onmouseup = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatey(0deg) scale(.8);';
+}
+DSLiteDPadDown.onmousedown = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatex(-5deg) scale(.8)';
+}
+DSLiteDPadDown.onmouseup = function() {
+    DSLiteDPad.style = 'transform: perspective(5em) rotatex(0deg) scale(.8);';
+}
+
+
+// SWITCH --------------------------------
+var nSwitch = document.querySelector('main > article:nth-of-type(3)');
+var nSwitchHomeButton = document.querySelector('main > article:nth-of-type(3) > section:nth-of-type(1) > section:nth-of-type(3) > ul > li:nth-of-type(4) button');
+var nSwitchMenuButton1 = document.querySelector('main > article:nth-of-type(3) > section:nth-of-type(2) ol li:nth-of-type(1) button');
+var nSwitchMenuButton2 = document.querySelector('main > article:nth-of-type(3) > section:nth-of-type(2) ol li:nth-of-type(2) button');
+var nSwitchMenuButton3 = document.querySelector('main > article:nth-of-type(3) > section:nth-of-type(2) ol li:nth-of-type(3) button');
+nSwitchHomeButton.addEventListener('mouseup', switchMenuOpen);
+nSwitchMenuButton3.addEventListener('click', switchMenuClose);
+
+// OPEN SWITCH MENU
+function switchMenuOpen() {
+    nSwitch.classList.add('menu-active');
+
+    setTimeout(function() {
+        nSwitch.classList.remove('device-visible');
+    }, 500);
+}
+
+// CLOSE SWITCH MENU
+function switchMenuClose() {
+    nSwitch.classList.remove('menu-active');
+    nSwitch.classList.add('device-visible');
+
+    // RESET ALL ANIMATIONS
+    resetAnimations();
+    // PLAY AUDIO ON DEVICE BOOT
+    bootSwitchAudio();
+}
+
+// PLAY AUDIO ON DEVICE BOOT
+function bootSwitchAudio() {
+    var switchBootAudio = new Audio('sounds/switch-boot-audio.mp3');
+    if (!nSwitch.classList.contains('menu-active')) {
+        console.log("Booting...")
+
+        setTimeout(function() {
+            switchBootAudio.play();
+            console.log("Booted")
+        }, 2300);
+    }
+}
+
 
 
 // SWITCH BETWEEN DEVICES --------------------------------
 // SWITCH TO GAMEBOY
-gameBoyMenuButton2.addEventListener('click', showDSLite);
-function showDSLite() {
-    DSLite.classList.remove('device-hidden');
-    DSLite.classList.add('device-visible');
-    gameBoy.classList.remove('menu-active');
-    gameBoy.classList.add('device-hidden');
-    
-    resetAnimations();
-    bootDSLiteAudio();
-}
-
-// SWITCH TO DS LITE
 DSLiteMenuButton1.addEventListener('click', showGameBoy);
+nSwitchMenuButton1.addEventListener('click', showGameBoy);
 function showGameBoy() {
     gameBoy.classList.remove('device-hidden');
     gameBoy.classList.add('device-visible');
     DSLite.classList.remove('menu-active');
     DSLite.classList.add('device-hidden');
+    nSwitch.classList.remove('menu-active');
+    nSwitch.classList.add('device-hidden');
 
     resetAnimations();
     bootGameBoyAudio();
 }
 
-// SWITCH TO SWITCH
-// ........
-// bezig...
-// ........
+// SWITCH TO DS LITE
+gameBoyMenuButton2.addEventListener('click', showDSLite);
+nSwitchMenuButton2.addEventListener('click', showDSLite);
+function showDSLite() {
+    DSLite.classList.remove('device-hidden');
+    DSLite.classList.add('device-visible');
+    gameBoy.classList.remove('menu-active');
+    gameBoy.classList.add('device-hidden');
+    nSwitch.classList.remove('menu-active');
+    nSwitch.classList.add('device-hidden');
+    
+    resetAnimations();
+    bootDSLiteAudio();
+}
 
+// SWITCH TO SWITCH
+gameBoyMenuButton3.addEventListener('click', showSwitch);
+DSLiteMenuButton3.addEventListener('click', showSwitch);
+function showSwitch() {
+    nSwitch.classList.remove('device-hidden');
+    nSwitch.classList.add('device-visible');
+    gameBoy.classList.remove('menu-active');
+    gameBoy.classList.add('device-hidden');
+    DSLite.classList.remove('menu-active');
+    DSLite.classList.add('device-hidden');
+    
+    resetAnimations();
+    bootSwitchAudio();
+}
 
 
 
@@ -164,46 +253,3 @@ if ('getBattery' in navigator){
         }
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// navigator.getBattery().then(function(battery) {
-//     var clientBatteryLevel = battery.level;
-//     var clientBatteryLevelExact = clientBatteryLevel * 100;
-//     console.log("Battery level: " + clientBatteryLevelExact);
-// });
-
-
-// navigator.getBattery().then(battery => {
-//     // ON BATTERY CHARGING STATUS CHANGE
-//     battery.onchargingchange = () => {
-//        console.log(battery.level);
-//        console.log(battery.chargingTime);
-//     }
-
-//     // ON BATTERY LEVEL CHANGE
-//     battery.onlevelchange = () => {
-//         // document.querySelector('#level').textContent = battery.level;
-    
-//         if(battery.charging) {
-//         console.log("Charging time: " + (battery.chargingTime / 60));
-//         }
-//         else {
-//         console.log("Discharging time: " + (battery.dischargingTime / 60));
-//         }
-//     };
-//  });
-
-
-
-
-
